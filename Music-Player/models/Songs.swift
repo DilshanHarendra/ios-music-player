d3eb63356185f758b62e33304077acc035559826
DilshanHarendra <harendracripto@gmail.com>
2021-12-27 14:07:44 +0530

    create song model

---
+++ Music-Player/models/Songs.swift
@@ -0,0 +1,16 @@
+//
+//  Songs.swift
+//  Music-Player
+//
+//  Created by Dilshan Harendra Perera on 2021-12-27.
+//
+
+import Foundation
+
+struct Song {
+    let name:String
+    let album:String
+    let artistName:String
+    let imageName:String
+    let trackName:String
+}
